cf3d530c1145a9ce406884700fa7dcd2570e1e0c
Saitore973 <saitorevg@gmail.com>
2022-04-17 00:05:05 +0300

    connect to postgress and migrate

--- hub/settings.py
+++ hub/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'around',
+        'USER': 'moringa',
+        'PASSWORD':'Access',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
