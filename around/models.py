5677410276f68cbff2d14de39512f5e45b33ee98
Saitore973 <saitorevg@gmail.com>
2022-04-17 00:20:41 +0300

    create a user model

--- around/models.py
+++ around/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
+
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_pic = CloudinaryField('image')
+    
--- hub/settings.py
+++ hub/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'around',
     'bootstrap5',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
