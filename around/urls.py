31108a0388d1cbe4c38a8560516aff0ef19bfc8f
Saitore973 <saitorevg@gmail.com>
2022-04-17 10:51:59 +0300

    display django registration form

--- around/urls.py
+++ around/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login")
 ]
--- around/views.py
+++ around/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def registerPage(request):
+    form = UserCreationForm()
+    context = {'form':form}
+    return render (request, 'all/register.html', context)
+def loginPage(request):
+    context = {}
+    return render(request, 'all/login.html', context)
