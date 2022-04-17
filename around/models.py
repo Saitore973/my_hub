5bb32ac08925eacb0a2576fdaecffe98e34a2145
Saitore973 <saitorevg@gmail.com>
2022-04-17 10:21:50 +0300

    create a NEIGHBOURHOOD  modeL

--- around/models.py
+++ around/models.py
@@ -10,3 +10,20 @@ class User(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     profile_pic = CloudinaryField('image')
     
+    def __str__(self):
+        return self.user.username
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.CharField(max_length=100)
+    image = CloudinaryField('image')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+
+    class Meta:
+        ordering = ['-created', '-updated']
+    
+    def __str__(self):
+        return self.title
+    
