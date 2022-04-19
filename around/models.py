b62440b241ebddccf811e2dc62384ecb61f50565
Saitore973 <saitorevg@gmail.com>
2022-04-19 12:33:03 +0300

    edit model

--- around/models.py
+++ around/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+
+from around.views import neighborhood
 # Create your models here.
 
 class Profile(models.Model):
@@ -9,6 +11,9 @@ class Profile(models.Model):
     email = models.EmailField(max_length=200)
     bio = models.TextField(max_length=500, blank=True)
     profile_pic = CloudinaryField( 'image',null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} profile'
     
     def __str__(self):
         return self.user.username
@@ -20,13 +25,19 @@ class Neighborhood(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
-
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
     class Meta:
         ordering = ['-created', '-updated']
     
     def __str__(self):
         return self.name
 
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+        
     @classmethod
     def search_by_name(cls,search_term):
         neighbours = cls.objects.filter(name__icontains=search_term)
@@ -38,3 +49,12 @@ class Business(models.Model):
     description = models.TextField(blank=True)
     neighbourhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='business')
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} Business'
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
--- around/tests.py
+++ around/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+# test profile class
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        # create a user
+        self.user = User.objects.create(
+            username='test_user',
+            full_name='john doe',
+        )
+
+        Profile.objects.create(
+            bio='test bio',
+            profilePhoto='https://res.cloudinary.com/james-m/image/upload/v1646636668/bbejbztp3i23jtfgoxzu.webp',
+            user=self.user
+        )
+
+    def test_bio(self):
+        profile = Profile.objects.get(user_id=self.user.id)
+        self.assertEqual(profile.bio, 'test bio')
+
